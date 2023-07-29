da57ddaa8d3625be94c9549e7afa71d64f273adf
TantoFaz <thiago.playgames15@gmail.com>
2023-07-28 22:09:22 -0300

    os.makedirs(os.path.dirname(file_path), exist_ok=True)

--- src/utils/api_request.py
+++ src/utils/api_request.py
@@ -1,6 +1,12 @@
 from fastapi import UploadFile
 import shutil
+import os
 
 def save_request_file(file_path: str, file: UploadFile) -> None:
+    os.makedirs(
+        name = os.path.dirname(file_path),
+        exist_ok = True
+    )
+
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
